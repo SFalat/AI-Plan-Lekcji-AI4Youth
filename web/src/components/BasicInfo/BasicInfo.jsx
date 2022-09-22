b86ad544bbded0234ef65948012a231359b3c14d
SFalat <szymon.falat@falat.pl>
2022-09-22 19:37:40 +0200

    Added teacher number input

--- web/src/components/BasicInfo/BasicInfo.jsx
+++ web/src/components/BasicInfo/BasicInfo.jsx
@@ -3,14 +3,52 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const StyledBasicInfo = styled.div`
-  display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  grid-auto-rows: 1fr;
+  display: flex;
+  flex-direction: column;
   height: 100vh;
+  align-items: center;
+  justify-content: center;
 `;
 
+const StyledOption = styled.div`
+  display: flex;
+  max-width: 20rem;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  &:not(:last-child) {
+    margin-bottom: 0.5rem;
+  }
+`;
+
+const StyledOptionInput = styled.input`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  border: none;
+  background-color: #14b1ae;
+  color: #fff;
+  font-size: 1rem;
+  font-family: inherit;
+  padding: 0.75rem 1rem;
+  text-decoration: none;
+  border-radius: 0.5rem;
+  :hover {
+    opacity: 0.9;
+  }
+`;
+
+
 function BasicInfo() {
-  return <StyledBasicInfo>123</StyledBasicInfo>;
+  return <StyledBasicInfo>
+    <StyledOption>
+    <label htmlFor="number_of_teachers">Ilość nauczycieli:</label>
+    <StyledOptionInput type="number" name="number_of_teachers"/>
+    </StyledOption>
+  </StyledBasicInfo>;
 }
 
 export default BasicInfo;
